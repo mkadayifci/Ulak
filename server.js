01f514fd962a4e030a56c659c03121fe6955c209
mkadayifci <mkadayifci@gmail.com>
2016-09-13 13:56:02 +0000

    ...

--- server.js
+++ server.js
@@ -21,13 +21,13 @@ var serialport = require('serialport');
    portName = '/dev/ttyUSB0';
 
 var myPort = new SerialPort(portName, {
-   baudRate: 9000,
+   baudRate: 9600,
    parser: serialport.parsers.readline("\n")
  });
 
 
 myPort.on('open', showPortOpen);
-myPort.on('data', sendSerialData);
+myPort.on('data', serialDataReceived);
 myPort.on('close', showPortClose);
 myPort.on('error', showError);
 
@@ -35,7 +35,7 @@ function showPortOpen() {
    console.log('port open. Data rate: ' + myPort.options.baudRate);
 }
  
-function sendSerialData(data) {
+function serialDataReceived(data) {
    console.log(data);
 }
  
